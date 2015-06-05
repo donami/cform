1fe4dff491a08d4f3d9adc5db8da0dc8f1313e1b
donami <d0namiz@gmail.com>
2015-06-05 14:25:50 +0200

    First commit

---
+++ src/HTMLForm/CForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace conat\HTMLForm;
+
+/**
+ * A utility class to easy creating and handling of forms
+ *
+ * @package CForm
+ */
+class CForm
+{
+
+
+    /**
+     * Constructor
+     *
+     * @param array $form     details for the form
+     * @param array $elements all the elements
+     */
+    public function __construct($form = [], $elements = [])
+    {
+    	die("HEJ");
+    }
+
+
+}
